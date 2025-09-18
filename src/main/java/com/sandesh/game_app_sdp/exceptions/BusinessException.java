3290a34ea4626e8e794cc94294f9577d0ff3b916
Sandesh_Naganasur <naganasursandesh7@gmail.com>
2025-09-18 15:17:30 +0530

    Delete src directory

--- src/main/java/com/sandesh/game_app_sdp/exceptions/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package com.sandesh.game_app_sdp.exceptions;
-
-public class BusinessException extends Exception {
-    public BusinessException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/sandesh/game_app_sdp/exceptions/IdNotPresentException.java
+++
@@ -1,7 +0,0 @@
-package com.sandesh.game_app_sdp.exceptions;
-
-public class IdNotPresentException extends Exception {
-    public IdNotPresentException(String message){
-        super(message);
-    }
-}
